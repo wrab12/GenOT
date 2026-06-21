7cdc5aa69d8a46b9f24fc9fadb6e0d4c6c230317
Rui (Ray) Wang <109090911+wrab12@users.noreply.github.com>
2026-06-21 11:18:22 -0500

    Import Encoder, DualEncoder, and Decoder in __init__.py

--- GenOT/__init__.py
+++ GenOT/__init__.py
@@ -1,2 +1,17 @@
+from .genot import Encoder, DualEncoder, Decoder
 from .utils import clustering
 from .preprocess import preprocess_adj, preprocess, construct_interaction, add_contrastive_label, get_feature, permutation, fix_seed
+
+__all__ = [
+    "Encoder",
+    "DualEncoder",
+    "Decoder",
+    "clustering",
+    "preprocess_adj",
+    "preprocess",
+    "construct_interaction",
+    "add_contrastive_label",
+    "get_feature",
+    "permutation",
+    "fix_seed",
+]
